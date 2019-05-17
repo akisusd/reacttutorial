ddb29d342c80453c2917282bcd4d1441b05f1b10
akisusd <akos.kovacs@clever-software-solutions.com>
2019-05-17 17:15:39 +0200

    Updates

---
+++ precache-manifest.e25e3789e44591f78a92e75b9b876df5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "502c5f661d93d4808382",
+    "url": "/akisusd/akisusd.github.io/reacttutorial/static/js/runtime~main.1d48546b.js"
+  },
+  {
+    "revision": "369a347b049e1f36fc6d",
+    "url": "/akisusd/akisusd.github.io/reacttutorial/static/js/main.6369ed16.chunk.js"
+  },
+  {
+    "revision": "4a532f803ac245692cfa",
+    "url": "/akisusd/akisusd.github.io/reacttutorial/static/js/2.9c115357.chunk.js"
+  },
+  {
+    "revision": "369a347b049e1f36fc6d",
+    "url": "/akisusd/akisusd.github.io/reacttutorial/static/css/main.6bd11a11.chunk.css"
+  },
+  {
+    "revision": "4a532f803ac245692cfa",
+    "url": "/akisusd/akisusd.github.io/reacttutorial/static/css/2.d790bf07.chunk.css"
+  },
+  {
+    "revision": "035b89260ad583003168614ae3b50562",
+    "url": "/akisusd/akisusd.github.io/reacttutorial/index.html"
+  }
+];
